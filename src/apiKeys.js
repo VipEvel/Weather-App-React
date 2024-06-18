480a788f61e7c6ce37c63748bb185808abfe2ed8
Vipin Sharma <vipdon1998@gmail.com>
2024-06-18 19:10:24 +0530

    Update apiKeys.js

--- src/apiKeys.js
+++ src/apiKeys.js
@@ -1,5 +1,5 @@
 // Visit https://api.openweathermap.org & then signup to get our API keys for free
 module.exports = {
-  key: "add your api key",
+  key: process.env.REACT_APP_API_KEY,
   base: "https://api.openweathermap.org/data/2.5/",
 };
